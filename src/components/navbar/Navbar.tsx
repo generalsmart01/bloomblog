6eed6c3f556ff01742a682ceeac8524760172f15
generalsmart01 <michaelbrainy19@gmail.com>
2024-01-05 01:14:54 +0100

    updated components

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,28 @@
+import Image from "next/image";
 import styles from "./navbar.module.css"
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
     return ( 
         <header className={styles.container}>
-           <div className={styles.social}></div> 
-           <div className={styles.logo}></div> 
-           <div className={styles.links}></div> 
+           <div className={styles.social}>
+            <Image src="/facebook.png" alt="facebook logo png" width={24} height={24} />
+            <Image src="/instagram.png" alt="facebook logo png" width={24} height={24} />
+            <Image src="/tiktok-icon-png.png" alt="facebook logo png" width={24} height={24} />
+            <Image src="/youtube-square-logo.png" alt="facebook logo png" width={24} height={24} /> 
+            </div> 
+           <div className={styles.logo}>
+            <Image src="/brandlogo.png" alt="bloomblog logo" width={120} height={50} />
+            </div> 
+           <div className={styles.links}>
+            <ThemeToggle />
+            <Link href="/" />Homepage
+            <Link href="/" />About
+            <Link href="/" />Contact
+            <AuthLinks />
+            </div> 
            <div className={styles.dashboard}></div> 
         </header>
      );
---
+++ src/components/themeToggle/ThemeToggle.tsx
@@ -0,0 +1,9 @@
+import styles from "./themetoggle.module.css"
+
+const ThemeToggle = () => {
+    return ( 
+        <div className={styles.container}>ThemeToggle</div>
+     );
+}
+ 
+export default ThemeToggle;
